35a176796f93c50eb03d6bbe20fd9a85f6564320
Harsh <patelharsh749005@gmail.com>
2025-06-03 12:19:28 +0530

    navbar as component

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,93 @@
+"use client"
+import { useState } from "react";
+
+export default function Navbar() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [visibility,setVisibility] = useState(false);
+
+  return (
+    <header
+      className=" border-b  p-4"
+      style={{
+        backgroundColor: "var(--background)",
+        borderBottom: "2px solid #2b2b2b",
+      }}
+    >
+      <div className="flex items-center justify-between">
+        <div className="flex items-center flex-1 max-w-2xl">
+          <h1 className="text-[#dededb] text-xl font-bold mr-8">GhostSafe</h1>
+          <div className="flex-1 relative">
+            <input
+              type="text"
+              placeholder="search my vault"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="w-full px-4 py-2 rounded-lg border focus:outline-none focus:border-zinc-300"
+              style={{
+                backgroundColor: "var(--background)",
+                border: "2px solid #2b2b2b",
+                color: "#dededb",
+              }}
+            />
+            <img
+              src="/images/search-2-line.svg"
+              className="absolute right-3 top-1/2 -translate-y-1/2 text-zinc-400 w-4 h-4 "
+            />
+          </div>
+        </div>
+        {/* Profile pic */}
+        <div className="flex items-center space-x-4">
+          {/* <button className="p-2 hover:bg-zinc-100 rounded-lg">
+                <span className="text-zinc-600">🔄</span>
+              </button> */}
+          <div
+            onMouseEnter={() => setVisibility(true)}
+            className=" w-10 h-10 object-cover rounded-lg"
+          >
+            <img
+              src="/images/nature.png"
+              alt=""
+              className="w-full h-full rounded-lg"
+            />
+            {/* <span className="text-zinc-600">👤</span> */}
+
+            {/* more options */}
+            {visibility && (
+              <div
+                onMouseEnter={() => setVisibility(true)}
+                onMouseLeave={() => setVisibility(false)}
+                style={{ backgroundColor: "var(--background)" }}
+                className="absolute right-4 mt-2 w-64 rounded-md shadow-lg  ring-1 ring-[#2b2b2b] ring-opacity-5 text-[#B0B0B0] z-50"
+              >
+                <div className="px-4 py-3 border-b border-zinc-700">
+                  <p className="text-sm font-semibold">Harsh Patel</p>
+                  <p className="text-sm text-zinc-400">
+                    patelharsh749005@gmail.com
+                  </p>
+                </div>
+
+                <div className="py-1">
+                  <button className="w-full text-left px-4 py-2 text-sm hover:bg-zinc-800">
+                    Account settings
+                  </button>
+                  <div className="relative group">
+                    <button className="w-full text-left px-4 py-2 text-sm hover:bg-zinc-800">
+                      Theme: <span className="text-zinc-400">System</span>
+                    </button>
+                    {/* Optional submenu */}
+                    {/* <div className="absolute left-full top-0 mt-0 hidden group-hover:block">
+                ...
+              </div> */}
+                  </div>
+                  <button className="w-full text-left px-4 py-2 text-sm hover:bg-zinc-800 text-red-400 font-medium cursor-pointer">
+                    Logout
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
