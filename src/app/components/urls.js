644d4673ad6f3bc370a9879fe8b94fabd48f7083
Harsh <patelharsh749005@gmail.com>
2025-06-07 14:02:52 +0530

    urls added

---
+++ src/app/components/urls.js
@@ -0,0 +1,414 @@
+export const urls = [
+  {
+    name: "abebooks.com",
+    icon: "https://www.abebooks.com/favicon.ico",
+    url: "https://www.abebooks.com/",
+  },
+  {
+    name: "about.me",
+    icon: "https://about.me/favicon.ico",
+    url: "https://about.me/",
+  },
+  {
+    name: "abc.com",
+    icon: "https://www.abc.com/favicon.ico",
+    url: "https://www.abc.com/",
+  },
+  {
+    name: "adobe.com",
+    icon: "https://www.adobe.com/favicon.ico",
+    url: "https://www.adobe.com/",
+  },
+  {
+    name: "airbnb.com",
+    icon: "https://www.airbnb.com/favicon.ico",
+    url: "https://www.airbnb.com/",
+  },
+  {
+    name: "alibaba.com",
+    icon: "https://www.alibaba.com/favicon.ico",
+    url: "https://www.alibaba.com/",
+  },
+  {
+    name: "amazon.com",
+    icon: "https://www.amazon.com/favicon.ico",
+    url: "https://www.amazon.com/",
+  },
+  {
+    name: "amd.com",
+    icon: "https://www.amd.com/favicon.ico",
+    url: "https://www.amd.com/",
+  },
+  {
+    name: "angellist.com",
+    icon: "https://angel.co/favicon.ico",
+    url: "https://angel.co/",
+  },
+  {
+    name: "aol.com",
+    icon: "https://www.aol.com/favicon.ico",
+    url: "https://www.aol.com/",
+  },
+  {
+    name: "asana.com",
+    icon: "https://asana.com/favicon.ico",
+    url: "https://asana.com/",
+  },
+  {
+    name: "ask.com",
+    icon: "https://www.ask.com/favicon.ico",
+    url: "https://www.ask.com/",
+  },
+  {
+    name: "atlassian.com",
+    icon: "https://www.atlassian.com/favicon.ico",
+    url: "https://www.atlassian.com/",
+  },
+  {
+    name: "att.com",
+    icon: "https://www.att.com/favicon.ico",
+    url: "https://www.att.com/",
+  },
+  {
+    name: "audible.com",
+    icon: "https://www.audible.com/favicon.ico",
+    url: "https://www.audible.com/",
+  },
+  {
+    name: "autodesk.com",
+    icon: "https://www.autodesk.com/favicon.ico",
+    url: "https://www.autodesk.com/",
+  },
+  {
+    name: "avast.com",
+    icon: "https://www.avast.com/favicon.ico",
+    url: "https://www.avast.com/",
+  },
+  {
+    name: "avg.com",
+    icon: "https://www.avg.com/favicon.ico",
+    url: "https://www.avg.com/",
+  },
+  {
+    name: "avira.com",
+    icon: "https://www.avira.com/favicon.ico",
+    url: "https://www.avira.com/",
+  },
+  {
+    name: "axios.com",
+    icon: "https://www.axios.com/favicon.ico",
+    url: "https://www.axios.com/",
+  },
+
+  {
+    name: "bbc.com",
+    icon: "https://www.bbc.com/favicon.ico",
+    url: "https://www.bbc.com/",
+  },
+  {
+    name: "bing.com",
+    icon: "https://www.bing.com/favicon.ico",
+    url: "https://www.bing.com/",
+  },
+  {
+    name: "blackberry.com",
+    icon: "https://www.blackberry.com/favicon.ico",
+    url: "https://www.blackberry.com/",
+  },
+  {
+    name: "booking.com",
+    icon: "https://www.booking.com/favicon.ico",
+    url: "https://www.booking.com/",
+  },
+  {
+    name: "buzzfeed.com",
+    icon: "https://www.buzzfeed.com/favicon.ico",
+    url: "https://www.buzzfeed.com/",
+  },
+
+  {
+    name: "cnn.com",
+    icon: "https://www.cnn.com/favicon.ico",
+    url: "https://www.cnn.com/",
+  },
+  {
+    name: "cisco.com",
+    icon: "https://www.cisco.com/favicon.ico",
+    url: "https://www.cisco.com/",
+  },
+  {
+    name: "citigroup.com",
+    icon: "https://www.citigroup.com/favicon.ico",
+    url: "https://www.citigroup.com/",
+  },
+  {
+    name: "coca-colacompany.com",
+    icon: "https://www.coca-colacompany.com/favicon.ico",
+    url: "https://www.coca-colacompany.com/",
+  },
+  {
+    name: "capgemini.com",
+    icon: "https://www.capgemini.com/favicon.ico",
+    url: "https://www.capgemini.com/",
+  },
+
+  {
+    name: "dell.com",
+    icon: "https://www.dell.com/favicon.ico",
+    url: "https://www.dell.com/",
+  },
+  {
+    name: "dropbox.com",
+    icon: "https://www.dropbox.com/favicon.ico",
+    url: "https://www.dropbox.com/",
+  },
+  {
+    name: "docusign.com",
+    icon: "https://www.docusign.com/favicon.ico",
+    url: "https://www.docusign.com/",
+  },
+  {
+    name: "danone.com",
+    icon: "https://www.danone.com/favicon.ico",
+    url: "https://www.danone.com/",
+  },
+  {
+    name: "dailymotion.com",
+    icon: "https://www.dailymotion.com/favicon.ico",
+    url: "https://www.dailymotion.com/",
+  },
+
+  {
+    name: "ebay.com",
+    icon: "https://www.ebay.com/favicon.ico",
+    url: "https://www.ebay.com/",
+  },
+  {
+    name: "espn.com",
+    icon: "https://www.espn.com/favicon.ico",
+    url: "https://www.espn.com/",
+  },
+  {
+    name: "etsy.com",
+    icon: "https://www.etsy.com/favicon.ico",
+    url: "https://www.etsy.com/",
+  },
+  {
+    name: "expedia.com",
+    icon: "https://www.expedia.com/favicon.ico",
+    url: "https://www.expedia.com/",
+  },
+  {
+    name: "evernote.com",
+    icon: "https://www.evernote.com/favicon.ico",
+    url: "https://www.evernote.com/",
+  },
+
+  {
+    name: "facebook.com",
+    icon: "https://www.facebook.com/favicon.ico",
+    url: "https://www.facebook.com/",
+  },
+  {
+    name: "ford.com",
+    icon: "https://www.ford.com/favicon.ico",
+    url: "https://www.ford.com/",
+  },
+  {
+    name: "fidelity.com",
+    icon: "https://www.fidelity.com/favicon.ico",
+    url: "https://www.fidelity.com/",
+  },
+  {
+    name: "fujitsu.com",
+    icon: "https://www.fujitsu.com/favicon.ico",
+    url: "https://www.fujitsu.com/",
+  },
+  {
+    name: "flex.com",
+    icon: "https://www.flex.com/favicon.ico",
+    url: "https://www.flex.com/",
+  },
+
+  {
+    name: "google.com",
+    icon: "https://www.google.com/favicon.ico",
+    url: "https://www.google.com/",
+  },
+  {
+    name: "github.com",
+    icon: "https://www.github.com/favicon.ico",
+    url: "https://www.github.com/",
+  },
+  {
+    name: "groupon.com",
+    icon: "https://www.groupon.com/favicon.ico",
+    url: "https://www.groupon.com/",
+  },
+  {
+    name: "gsk.com",
+    icon: "https://www.gsk.com/favicon.ico",
+    url: "https://www.gsk.com/",
+  },
+  {
+    name: "godaddy.com",
+    icon: "https://www.godaddy.com/favicon.ico",
+    url: "https://www.godaddy.com/",
+  },
+
+  {
+    name: "hp.com",
+    icon: "https://www.hp.com/favicon.ico",
+    url: "https://www.hp.com/",
+  },
+  {
+    name: "honda.com",
+    icon: "https://www.honda.com/favicon.ico",
+    url: "https://www.honda.com/",
+  },
+  {
+    name: "hulu.com",
+    icon: "https://www.hulu.com/favicon.ico",
+    url: "https://www.hulu.com/",
+  },
+  {
+    name: "hertz.com",
+    icon: "https://www.hertz.com/favicon.ico",
+    url: "https://www.hertz.com/",
+  },
+  {
+    name: "hsbc.com",
+    icon: "https://www.hsbc.com/favicon.ico",
+    url: "https://www.hsbc.com/",
+  },
+
+  {
+    name: "ibm.com",
+    icon: "https://www.ibm.com/favicon.ico",
+    url: "https://www.ibm.com/",
+  },
+  {
+    name: "intel.com",
+    icon: "https://www.intel.com/favicon.ico",
+    url: "https://www.intel.com/",
+  },
+  {
+    name: "instagram.com",
+    icon: "https://www.instagram.com/favicon.ico",
+    url: "https://www.instagram.com/",
+  },
+  {
+    name: "indeed.com",
+    icon: "https://www.indeed.com/favicon.ico",
+    url: "https://www.indeed.com/",
+  },
+  {
+    name: "ikea.com",
+    icon: "https://www.ikea.com/favicon.ico",
+    url: "https://www.ikea.com/",
+  },
+
+  {
+    name: "jpmorganchase.com",
+    icon: "https://www.jpmorganchase.com/favicon.ico",
+    url: "https://www.jpmorganchase.com/",
+  },
+  {
+    name: "johnsoncontrols.com",
+    icon: "https://www.johnsoncontrols.com/favicon.ico",
+    url: "https://www.johnsoncontrols.com/",
+  },
+  {
+    name: "jaguar.com",
+    icon: "https://www.jaguar.com/favicon.ico",
+    url: "https://www.jaguar.com/",
+  },
+  {
+    name: "jetblue.com",
+    icon: "https://www.jetblue.com/favicon.ico",
+    url: "https://www.jetblue.com/",
+  },
+  {
+    name: "jcrew.com",
+    icon: "https://www.jcrew.com/favicon.ico",
+    url: "https://www.jcrew.com/",
+  },
+
+  {
+    name: "kelloggcompany.com",
+    icon: "https://www.kelloggcompany.com/favicon.ico",
+    url: "https://www.kelloggcompany.com/",
+  },
+  {
+    name: "kpmg.com",
+    icon: "https://www.kpmg.com/favicon.ico",
+    url: "https://www.kpmg.com/",
+  },
+  {
+    name: "kraftheinzcompany.com",
+    icon: "https://www.kraftheinzcompany.com/favicon.ico",
+    url: "https://www.kraftheinzcompany.com/",
+  },
+  {
+    name: "kohls.com",
+    icon: "https://www.kohls.com/favicon.ico",
+    url: "https://www.kohls.com/",
+  },
+  {
+    name: "kia.com",
+    icon: "https://www.kia.com/favicon.ico",
+    url: "https://www.kia.com/",
+  },
+
+  {
+    name: "linkedin.com",
+    icon: "https://www.linkedin.com/favicon.ico",
+    url: "https://www.linkedin.com/",
+  },
+  {
+    name: "lenovo.com",
+    icon: "https://www.lenovo.com/favicon.ico",
+    url: "https://www.lenovo.com/",
+  },
+  {
+    name: "lufthansa.com",
+    icon: "https://www.lufthansa.com/favicon.ico",
+    url: "https://www.lufthansa.com/",
+  },
+  {
+    name: "lg.com",
+    icon: "https://www.lg.com/favicon.ico",
+    url: "https://www.lg.com/",
+  },
+  {
+    name: "levis.com",
+    icon: "https://www.levis.com/favicon.ico",
+    url: "https://www.levis.com/",
+  },
+
+  {
+    name: "microsoft.com",
+    icon: "https://www.microsoft.com/favicon.ico",
+    url: "https://www.microsoft.com/",
+  },
+  {
+    name: "mastercard.com",
+    icon: "https://www.mastercard.com/favicon.ico",
+    url: "https://www.mastercard.com/",
+  },
+  {
+    name: "mcdonalds.com",
+    icon: "https://www.mcdonalds.com/favicon.ico",
+    url: "https://www.mcdonalds.com/",
+  },
+  {
+    name: "mozilla.org",
+    icon: "https://www.mozilla.org/favicon.ico",
+    url: "https://www.mozilla.org/",
+  },
+  {
+    name: "morganstanley.com",
+    icon: "https://www.morganstanley.com/favicon.ico",
+    url: "https://www.morganstanley.com/",
+  },
+];
