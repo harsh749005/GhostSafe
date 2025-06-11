e4846ccaa8f33280694da79c383c2692f0fc13ce
Harsh <patelharsh749005@gmail.com>
2025-06-11 14:17:51 +0530

    edit notes backend

--- app/api/passwords/editpasswords/route.js
+++ app/api/passwords/editpasswords/route.js
@@ -4,10 +4,16 @@ import { NextResponse } from "next/server";
 
 export async function PUT(req) {
   const { id, name, username, password, url } = await req.json();
-  await sql`
+  try{
+    
+    await sql`
     UPDATE ghostsafe_passworddata
     SET name = ${name}, username = ${username}, password = ${password}, url = ${url}
     WHERE id = ${id}
-  `;
-  return NextResponse.json({ success: true },{status:200});
+    `;
+    return NextResponse.json({ success: true },{status:200});
+  }catch (error) {
+    console.error("Delete error:", error);
+    return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
+    }
 }
