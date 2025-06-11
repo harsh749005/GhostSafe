122d658e97804b8b1f5479297e38c052524961b8
Harsh <patelharsh749005@gmail.com>
2025-06-11 14:17:19 +0530

    user can delete notes

---
+++ app/api/notes/deletenotes/route.js
@@ -0,0 +1,20 @@
+import { sql } from "../../../lib/db";
+import { NextResponse } from "next/server";
+
+export async function DELETE(request) {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+
+    if(!id){
+        return NextResponse.json({ error: "ID is required" }, { status: 400 });
+    }
+  
+  
+    try{
+         await sql`DELETE FROM ghostsafe_securenotes WHERE id=${id}`;
+        return NextResponse.json({ success: true }, { status: 200 });
+    }catch (error) {
+    console.error("Delete error:", error);
+    return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
+  }  
+}
