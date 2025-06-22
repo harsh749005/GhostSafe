44d15eada957584501ef93c72871821cdf7f011e
Harsh <patelharsh749005@gmail.com>
2025-06-22 09:13:46 +0530

    token and cookie are updated

--- app/api/profile/editData/route.js
+++ app/api/profile/editData/route.js
@@ -2,48 +2,40 @@ import { cookies } from "next/headers";
 import { sql } from "../../../lib/db";
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
+import jwt from "jsonwebtoken";
 
+const JWT_SECRET = process.env.JWT_SECRET;
 export async function PUT(req) {
   try {
     const { name, email } = await req.json();
-    console.log("Received body", name);
+
     const cookieStore = cookies();
     const userCookieRaw = cookieStore.get("user")?.value;
     const userCookie = JSON.parse(userCookieRaw);
     const id = userCookie.id;
-    console.log(userCookie);
-    // Example update (you'll need user ID or email to match user)
 
-    // const result =
-    //   await sql`SELECT * FROM ghostsafe_user WHERE email = ${email}`;
-    // const user = result[0];
-
-    // if (user) {
-    //   return NextResponse.json(
-    //     { error: "Email already exists" },
-    //     { status: 404 }
-    //   );
-    // }
     await sql`UPDATE ghostsafe_user SET name = ${name}, email = ${email} WHERE id = ${id}`;
 
-    const token = req.cookies.get("token")?.value;
-    console.log(token);
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+    const token = jwt.sign({ id, email, name }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const secret = new TextEncoder().encode(JWT_SECRET);
     const { payload } = await jwtVerify(token, secret);
 
-    const response = NextResponse.next();
-
-    // Store decoded data in a cookie (can also use headers)
+    const response = NextResponse.json({ success: true }, { status: 200 });
     response.cookies.set("user", JSON.stringify(payload), {
-      httpOnly: false, // Make it accessible to JS on frontend
+      httpOnly: false,
+      path: "/",
+    });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 7,
       path: "/",
     });
 
-    return NextResponse.json({ success: true },{status : 200});
+    return response;
   } catch (err) {
     console.error(err);
     return NextResponse.json(
